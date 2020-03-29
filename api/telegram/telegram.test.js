9869e847f3272eac372a8012af157d18f78b6c4b
@milesibastos <antonio@milesibastos.com>
2020-03-29 15:57:16 -0300

    test: token should be valid

--- api/telegram/telegram.test.js
+++ api/telegram/telegram.test.js
@@ -15,8 +15,16 @@ test('token is required', () => {
 test('token not match', () => {
   process.env.TELEGRAM_TOKEN = 'foo'
   const req = { query: { token: 'bar' } }
-  const res = { json: jest.fn }
+  const res = { json: jest.fn() }
   expect(() => {
     api(req, res)
   }).toThrowError(/token not match/)
+})
+
+test('token should be valid', () => {
+  process.env.TELEGRAM_TOKEN = 'foo:bar'
+  const req = { query: { token: 'foo:bar' } }
+  const res = { json: jest.fn() }
+  api(req, res)
+  expect(res.json).toHaveBeenCalled()
 })
