4e99e45822ad5de98fa869954667e716442d2aed
@milesibastos <antonio@milesibastos.com>
2020-03-29 14:37:42 -0300

    test: token not match

--- api/telegram/[token].js
+++ api/telegram/[token].js
@@ -1,6 +1,7 @@
 module.exports = (req, res) => {
   const { token } = req.query
   if (!token) throw new Error('token is required')
+  if (token !== process.env.TELEGRAM_TOKEN) throw new Error('token not match')
   res.json({
     body: req.body,
     query: req.query,
--- api/telegram/telegram.test.js
+++ api/telegram/telegram.test.js
@@ -11,3 +11,12 @@ test('token is required', () => {
     api(req, res)
   }).toThrowError(/token is required/)
 })
+
+test('token not match', () => {
+  process.env.TELEGRAM_TOKEN = 'foo'
+  const req = { query: { token: 'bar' } }
+  const res = { json: jest.fn }
+  expect(() => {
+    api(req, res)
+  }).toThrowError(/token not match/)
+})
