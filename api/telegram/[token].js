fdb95972fe810b824143e74830346f99288ab8fe
Antonio Milesi Bastos <antonio@milesibastos.com>
2020-03-31 01:47:21 -0300

    migrate to telegraf 👍
    Signed-off-by: Antonio Milesi Bastos <antonio@milesibastos.com>

--- api/telegram/[token].js
+++ api/telegram/[token].js
@@ -1,13 +1,14 @@
 const Telegraf = require("telegraf");
 const TelegrafI18n = require("telegraf-i18n");
 const Extra = require("telegraf/extra");
-const Markup = require("telegraf/markup");
+// const Markup = require("telegraf/markup");
 
 const path = require("path");
 const logger = require("pino")();
 const loggerHttp = require("pino-http")({ logger });
 
 const repository = require("../../repository");
+const geocoding = require("../../repository/geocoding");
 
 const i18n = new TelegrafI18n({
   defaultLanguage: "en",
@@ -20,8 +21,15 @@ const i18n = new TelegrafI18n({
 });
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
-// bot.use(Telegraf.memorySession())
 bot.use(i18n.middleware());
+bot.use((ctx, next) => {
+  logger.info(ctx.message);
+  return next();
+});
+
+bot.catch((err, ctx) => {
+  logger.error(`Ooops, encountered an error for ${ctx.updateType}`, err);
+});
 
 bot.start(({ i18n, replyWithHTML }) =>
   replyWithHTML(
@@ -48,12 +56,33 @@ bot.hears(TelegrafI18n.match("worldwide"), ({ i18n, reply }) =>
 );
 
 bot.on("text", (ctx) => ctx.reply(ctx.message.text));
+bot.on("location", ({ i18n, message: { location }, reply }) =>
+  geocoding(location.latitude, location.longitude).then(
+    ({ country, locality }) =>
+      repository.country(country.long_name).then((countryStatistics) =>
+        repository.locality(locality.short_name).then((statistics) => {
+          const msg = `${country.long_name}\n${i18n.t("statistics", {
+            locale: i18n.locale(),
+            ...countryStatistics,
+          })}\n${locality.long_name}\n${i18n.t("statistics", {
+            locale: i18n.locale(),
+            ...cases,
+            ...statistics,
+          })}`;
+          return reply(msg);
+        })
+      )
+  )
+);
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   loggerHttp(req, res);
   const { token } = req.query;
   if (!token) throw new Error("token is required");
   if (token !== process.env.TELEGRAM_TOKEN) throw new Error("token not match");
 
-  bot.handleUpdate(req.body, res);
+  await bot.handleUpdate(req.body, res);
+  res.end();
 };
+
+const cases = { cases: 0, confirmed: 0, deaths: 0, recovered: 0 };
