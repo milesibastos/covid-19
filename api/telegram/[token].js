b40ad7769094e95c84fe1307b8ba30188eac9369
Antonio Milesi Bastos <antonio@milesibastos.com>
2020-03-30 21:57:17 -0300

    setup: telegraf-i18n

--- api/telegram/[token].js
+++ api/telegram/[token].js
@@ -1,10 +1,28 @@
 const Telegraf = require('telegraf')
+const TelegrafI18n = require('telegraf-i18n')
+const { Extra } = Telegraf
+
+const path = require('path')
 const logger = require('pino')()
 const loggerHttp = require('pino-http')({ logger })
 
+const i18n = new TelegrafI18n({
+  defaultLanguage: 'en',
+  defaultLanguageOnMissing: true,
+  directory: path.resolve(__dirname, 'locales'),
+  templateData: {
+    pluralize: TelegrafI18n.pluralize,
+    uppercase: value => value.toUpperCase()
+  }
+})
+
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me your location'))
+// bot.use(Telegraf.memorySession())
+bot.use(i18n.middleware())
+
+bot.start(({ i18n, replyWithHTML }) => replyWithHTML(i18n.t('greeting')))
+
+bot.help(TelegrafI18n.reply('help', Extra.HTML()))
 
 module.exports = (req, res) => {
   loggerHttp(req, res)
