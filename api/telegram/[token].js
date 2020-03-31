9f4dda4d76f9077e513c1ca1ba7fe1ff4f8ae603
Antonio Milesi Bastos <antonio@milesibastos.com>
2020-03-30 23:47:00 -0300

    setup: keyboard

--- api/telegram/[token].js
+++ api/telegram/[token].js
@@ -1,6 +1,7 @@
 const Telegraf = require("telegraf");
 const TelegrafI18n = require("telegraf-i18n");
-const { Extra } = Telegraf;
+const Extra = require("telegraf/extra");
+const Markup = require("telegraf/markup");
 
 const path = require("path");
 const logger = require("pino")();
@@ -20,17 +21,27 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 // bot.use(Telegraf.memorySession())
 bot.use(i18n.middleware());
 
-bot.start(({ replyWithSticker }) =>
-  replyWithSticker(
-    "CAACAgIAAxkBAANvXoIVU0H0D9p26ksAAeGsUiC1gRV6AAJaAAPANk8TC_wPT9xGGeEYBA"
+bot.start(({ i18n, replyWithHTML }) =>
+  replyWithHTML(
+    i18n.t("greeting"),
+    Extra.markup((markup) => {
+      return markup
+        .resize()
+        .keyboard([
+          i18n.t("worldwide"),
+          markup.locationRequestButton(i18n.t("local")),
+        ]);
+    })
   )
 );
 
 bot.help(TelegrafI18n.reply("help", Extra.HTML()));
 
+bot.on("text", (ctx) => ctx.reply(ctx.message.text));
+
 module.exports = (req, res) => {
   loggerHttp(req, res);
-  const { token, ...query } = req.query;
+  const { token } = req.query;
   if (!token) throw new Error("token is required");
   if (token !== process.env.TELEGRAM_TOKEN) throw new Error("token not match");
 
